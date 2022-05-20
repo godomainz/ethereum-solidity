c1e5abed2d3ce060b3700289e8d3d3b37efd0032
Akila Mendis <akila@oneit.com.au>
2022-05-20 11:29:58 +0530

    53. Verifying the Initial Message

--- section_2/inbox/src/test/Inbox.spec.ts
+++ section_2/inbox/src/test/Inbox.spec.ts
@@ -1,4 +1,4 @@
-import { ok } from 'assert';
+import { ok, strictEqual } from 'assert';
 import { provider } from 'ganache';
 import Web3 from 'web3';
 import { beforeEach } from 'mocha';
@@ -8,15 +8,21 @@ import { Contract } from 'web3-eth-contract';
 const web3 = new Web3(provider() as any);
 let accounts:string[];
 let inbox: Contract;
+const INITIAL_MESSAGE: string = 'Hi There!';
+
 beforeEach(async ()=>{
     accounts = await web3.eth.getAccounts();
 
-    inbox = await new web3.eth.Contract(JSON.parse(interface_str)).deploy({data: bytecode, arguments: ['Hi There']})
+    inbox = await new web3.eth.Contract(JSON.parse(interface_str)).deploy({data: bytecode, arguments: [INITIAL_MESSAGE]})
     .send({ from: accounts[0], gas: 10000000 });
 });
 
 describe("Inbox",()=>{
-    it('Deploys a contract', ()=>{
+    it('should deploy a contract', ()=>{
         ok(inbox.options.address);
+    });
+    it('should have a default message', async ()=>{
+        const message = await inbox.methods.message().call();
+        strictEqual(message, INITIAL_MESSAGE);
     });
 });
