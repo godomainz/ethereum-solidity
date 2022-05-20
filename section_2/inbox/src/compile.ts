a79845f3bbd7c994b35f61d44b3c003b2ec74960
Akila Mendis <akila@oneit.com.au>
2022-05-20 08:11:05 +0530

    50. Deployment with Web3

--- section_2/inbox/src/compile.ts
+++ section_2/inbox/src/compile.ts
@@ -4,5 +4,6 @@ import { readFileSync } from 'fs';
 
 const inboxPath = resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = readFileSync(inboxPath, 'utf8');
-
-export default solc.compile(source,1).contracts[':Inbox'];
+const bytecode= solc.compile(source,1).contracts[':Inbox'].bytecode;
+const interface_str= solc.compile(source,1).contracts[':Inbox'].interface;
+export { bytecode, interface_str };
--- section_2/inbox/src/test/Inbox.spec.ts
+++ section_2/inbox/src/test/Inbox.spec.ts
@@ -2,15 +2,21 @@ import * as assert from 'assert';
 import { provider } from 'ganache';
 import Web3 from 'web3';
 import { beforeEach } from 'mocha';
+import { bytecode, interface_str } from '../compile';
+import { Contract } from 'web3-eth-contract';
 
 const web3 = new Web3(provider() as any);
 let accounts:string[];
+let inbox: Contract;
 beforeEach(async ()=>{
     accounts = await web3.eth.getAccounts();
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface_str)).deploy({data: bytecode, arguments: ['Hi There']})
+    .send({ from: accounts[0], gas: 10000000 });
 });
 
 describe("Inbox",()=>{
     it('Deploys a contract', ()=>{
-        console.log(accounts);
+        console.log(inbox);
     });
 });
