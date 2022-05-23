dfda44ea8eef190122cd10bf9d8708da50a8a145
Akila Mendis <akila@oneit.com.au>
2022-05-23 09:30:11 +0530

    77. Pseudo Random Number Generator

--- section_3/contracts/Lottery.sol
+++ section_3/contracts/Lottery.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.4.17 < 0.9.0;
 contract Lottery {
     address public manager;
     address[] public players;
+    uint private randNonce = 0;
 
     constructor() {
         manager = msg.sender;
@@ -12,5 +13,10 @@ contract Lottery {
     function enter() public payable{
         require(msg.value > .01 ether);
         players.push(msg.sender);
+    }
+
+    function random() private returns (uint) {
+        randNonce++;
+        return uint(keccak256(abi.encodePacked(block.difficulty, (block.timestamp * randNonce), players)));
     }
 }
